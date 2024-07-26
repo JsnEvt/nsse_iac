73b8df1461ab7de192f989d4fd17e34f2d5f62dc
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-07-26 10:45:50 -0300

    Remote backend/DynamoDB/Locking/S3

--- main.tf
+++ main.tf
@@ -6,3 +6,22 @@ provider "aws" {
   }
 }
 
+//ISSO E PARA ATIVAR O STATE LOCKING NA TABELA DYNAMO PARA 
+//QUE EM AMBIENTE COLABORATIVO, NAO SEJA POSSIVEL RODAR O APPLY AO MESMO
+//TEMPO POR OUTRA PESSOA. (E UM TRAVAMENTO PARA EVITAR CORRUPCAO DE DADOS do *.tfstate)
+//para que o tfstate saia do local e va para o S3, precisamos
+//adicionar o bucket no codigo main.tf
+//executando previamente o apply com o codigo do backend.terraform 
+//o bucket e a tabela dynamo ja foram criadas.
+
+//o comando terraform init -migrate-state transfere o arquivo *.tfstate para o S3
+
+terraform {
+  backend "s3" {
+    bucket         = "nsse-terraform-state-bucket"
+    key            = "networking/terraform.tfstate" //dara erro, caso nao seja criada primeiro. (comentar a linha)
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "nsse-terraform-state-lock-table"
+  }
+}
