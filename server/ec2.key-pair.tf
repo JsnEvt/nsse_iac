ad899301a280808c5d8cd7f01b5ae138946e8575
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-07-27 16:34:23 -0300

    Secutity groups (SSH)

--- server/ec2.key-pair.tf
+++ server/ec2.key-pair.tf
@@ -6,4 +6,5 @@ resource "tls_private_key" "this" {
 resource "aws_key_pair" "this" {
   key_name   = var.ec2_resources.key_pair_name
   public_key = tls_private_key.this.publi_key_openssh
+  tags       = merge(var.tags, { name = var.ec2_resources.key_pair_name })
 }
