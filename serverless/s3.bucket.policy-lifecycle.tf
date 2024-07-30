cb6a10829db5fe351d9d6388d5be3d768c82b9d8
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-07-30 15:40:50 -0300

    s3 - Policy Lifecycle

---
+++ serverless/s3.bucket.policy-lifecycle.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket_lifecycle_configuration" "nsse_intelligent_tiering" {
+  bucket = aws_s3_bucket.nsse.id
+
+  rule {
+    id     = "MovingToIntellignetTiering"
+    status = "Enabled"
+
+    transition {
+      days          = 0
+      storage_class = "INTELLIGENT_TIERING"
+    }
+    noncurrent_version_transition {
+      noncurrent_days = 0
+      storage_class   = "INTELLIGENT_TIERING"
+    }
+  }
+}
