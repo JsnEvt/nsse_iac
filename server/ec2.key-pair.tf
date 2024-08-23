180bdfd23c4805672a3d37c6c394434f9be40cc0
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-08-23 09:22:39 -0300

    Instances - Adding key-pair info for better understanding

--- server/ec2.key-pair.tf
+++ server/ec2.key-pair.tf
@@ -1,3 +1,8 @@
+//a criacao desta chave servira para acesso remoto as instancias
+//esta criacao ocorrera durante o provisionamento da infraestrutura
+//no site do terraform, usamos o TLS(novo provider) para informar o algoritmo de
+//criptografia RSA
+
 resource "tls_private_key" "this" {
   algorithm = "RSA"
   rsa_bits  = 4096
--- server/outputs.tf
+++ server/outputs.tf
@@ -1,3 +1,10 @@
+//para gerar a chave privada que sera instalada na maquina local 
+//para acesso remoto as instancias
+
+//terraform output -raw key_pair_private_key exibira o conteudo da chave privada
+//com o output acima seguido do "> nome_da_chave.pem", sera gerado um arquivo 
+//com a chave privada para instalacao dentro da maquina local
+
 output "key_pair_private_key" {
   sensitive = true
   value     = tls_private_key.this.private_key_pem
