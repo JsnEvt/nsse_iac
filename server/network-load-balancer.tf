fe8ced74165582170923280a197200a51c1aea43
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-10-07 19:16:22 -0300

    NLB - Security Group

--- server/network-load-balancer.tf
+++ server/network-load-balancer.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "nlb_control_plane" {
   internal           = var.network_load_balancer.internal
   load_balancer_type = var.network_load_balancer.load_balancer_type
   subnets            = data.aws_subnets.private_subnets.ids
+  security_groups    = [aws_security_group.control_plane.id]
 
   tags = var.tags
 }
