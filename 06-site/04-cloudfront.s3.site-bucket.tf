ce4e832a231ace758052ad26b5afdec603abf938
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2025-07-21 16:24:32 -0300

    Cloudfront a2

---
+++ 06-site/04-cloudfront.s3.site-bucket.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "site" {
+  bucket = var.cloudfront.s3_site_bucket_name
+}
+
---
+++ 06-site/04-cloudfront.s3.site-logs-bucket.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "site_logs" {
+  bucket = var.cloudfront.s3_site_logs_bucket_name
+}
+
