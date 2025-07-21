5a969e59c139348ff5ef60789977a3bb89846b7a
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2025-07-21 17:05:44 -0300

    Cloudfront Distribution 1

---
+++ 06-site/06-cloudfront.distribution.tf
@@ -0,0 +1,32 @@
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id = aws_s3_bucket.site.bucket_regional_domain_name
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.site.bucket_regional_domain_name
+
+    #Managed-CachingOptimized
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    viewer_protocol_policy = "redirect_to_https"
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
