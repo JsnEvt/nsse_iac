ccbb544acd8e00524abbd597eb4af7d979ab3952
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2025-07-22 16:02:56 -0300

    Cloudfront - Custom Domain

--- 06-site/06-cloudfront.distribution.tf
+++ 06-site/06-cloudfront.distribution.tf
@@ -1,24 +1,23 @@
 resource "aws_cloudfront_distribution" "this" {
+  enabled             = var.cloudfront.enabled
+  default_root_object = var.cloudfront.default_root_object
+  price_class         = var.cloudfront.price_class
+  aliases             = [var.cloudfront.domain]
   origin {
     domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.this.id
     origin_id                = aws_s3_bucket.site.bucket_regional_domain_name
   }
 
-  enabled             = true
-  default_root_object = "index.html"
-
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
+    allowed_methods  = var.cloudfront.default_cache_behavior.allowed_methods
+    cached_methods   = var.cloudfront.default_cache_behavior.cached_methods
     target_origin_id = aws_s3_bucket.site.bucket_regional_domain_name
 
     #Managed-CachingOptimized
-    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-    viewer_protocol_policy = "redirect_to_https"
+    cache_policy_id        = var.cloudfront.default_cache_behavior.cache_policy_id
+    viewer_protocol_policy = var.cloudfront.default_cache_behavior.viewer_protocol_policy
   }
-
-  price_class = "PriceClass_All"
 
   restrictions {
     geo_restriction {
@@ -27,6 +26,10 @@ resource "aws_cloudfront_distribution" "this" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = data.aws_acm_certificate.this.arn
+    ssl_support_method  = "sni-only" #extensao do protocolo TLS / IP DEDICADO POR EDGE LOCATION 
+    #no caso de gerenciamento de dominio por edge location - seria muito caro
+    #entao a resolucao se dara de forma economica atraves do SNI - SERVER NAME INDICATION
+    #sendo compartilhado com outros dominios. 
   }
 }
